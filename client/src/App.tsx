ac14354372d908ed7e7dd6d2f7e6a0dbd03af27a
Aman <amankunalbharti2003@gmail.com>
2024-07-20 16:44:53 +0530

    testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,6 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
         HELLO my name is Aman
         i am a frontend engineer
+        3rd year btech student
       </p>
     </>
   )
