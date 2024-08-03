05fe5190b5f75c1dc04fd568ad3a6e33a0a2ebfe
anasahmd <anasahmad0239@gmail.com>
2024-08-03 21:41:35 +0530

    Add City route and controller

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
-const { DB_NAME } = require('../constants');
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
 
 mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
-	const connectionInstance = await mongoose.connect(
-		`${process.env.MONGODB_URI}/${DB_NAME}`
-	);
-	console.log('DB Connected! HOST: ' + connectionInstance.connection.host);
+	try {
+		const connectionInstance = await mongoose.connect(
+			`${process.env.MONGODB_URI}/${DB_NAME}`
+		);
+		console.log('DB Connected! HOST: ' + connectionInstance.connection.host);
+	} catch (error) {
+		console.log('MongoDB connection failed!', error);
+		process.exit(1);
+	}
 };
 
-module.exports = { connectDB };
+export default connectDB;
---
+++ server/models/Category.model.js
@@ -0,0 +1,6 @@
+import mongoose, { Schema } from 'mongoose';
+
+const categorySchema = new Schema({
+	name: { type: String, required: true },
+  
+});
