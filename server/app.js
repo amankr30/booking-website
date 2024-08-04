b92442eed9b63b5a5e29e0cf16148f6c59403da1
anasahmd <anasahmad0239@gmail.com>
2024-08-04 20:08:39 +0530

    Make filter capsule dynamic

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+		credentials: true,
+	})
+);
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 
 import cityRouter from './routes/city.route.js';
+import categoryRouter from './routes/category.route.js';
 
 app.use('/api/v1/city', cityRouter);
+app.use('/api/v1/category', categoryRouter);
 
 export { app };
