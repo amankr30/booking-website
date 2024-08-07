2150bfa69004a77e5bdb39d6bbcddf64aea859ed
Aman <amankunalbharti2003@gmail.com>
2024-08-08 01:36:44 +0530

    solving colflict issue

--- client/src/component/Body/Body.tsx
+++ client/src/component/Body/Body.tsx
@@ -4,22 +4,17 @@ import Card from "./Card";
 
 function Body() {
   return (
-    <div className="container w-full px-20 flex justify-start ">
-      <div className="filters w-[40%] flex flex-col justify-start">
-    <div className="container w-full px-20 flex justify-start ">
+    <div className="container w-full px-20 flex">
       <div className="filters w-[40%] flex flex-col justify-start">
         <div className="heading font-semibold lg:text-3xl md:text-2xl mb-5 text-left">
           <h1>Filters</h1>
         </div>
         <div className="options flex flex-col gap-y-3">
-          {/* <div className="pick-date w-full">
-          {/* <div className="pick-date w-full">
-            <DatePicker />
-          </div> */}
-          <div><Dropdown /></div>
+          {/* <DatePicker /> */}
+          <Dropdown />
         </div>
       </div>
-      <div className="events w-full flex flex-col text-left justify-start flex-wrap gap-5">
+      <div className="events w-[60%] flex flex-col text-left justify-start flex-wrap gap-5">
         <div className="heading font-semibold lg:text-3xl md:text-2xl">
           Events in Kolkata
         </div>
