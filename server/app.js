56f087b2f169951c08ddc4b391dd8a3f34d2f616
anasahmd <anasahmad0239@gmail.com>
2024-08-09 13:28:40 +0530

    Customized api error handler

--- server/app.js
+++ server/app.js
@@ -16,8 +16,16 @@ app.use(express.static('public'));
 
 import cityRouter from './routes/city.route.js';
 import categoryRouter from './routes/category.route.js';
+import errorHandler from './utils/errorHandler.js';
+import { ApiError } from './utils/ApiError.js';
 
 app.use('/api/v1/city', cityRouter);
 app.use('/api/v1/category', categoryRouter);
+
+app.use((req, res, next) => {
+	next(new ApiError(404, 'Not Found'));
+});
+
+app.use(errorHandler);
 
 export { app };
--- server/models/Show.model.js
+++ server/models/Show.model.js
@@ -2,15 +2,20 @@ import mongoose, { Schema } from 'mongoose';
 
 const showSchema = new Schema(
 	{
-		name: { type: String, required: true },
-		coverImage: { type: String },
-		organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+		name: { type: String, required: true, trim: true },
+		coverImage: {
+			public_id: { type: String, required: true },
+			url: { type: String, required: true },
+		},
 		city: { type: Schema.Types.ObjectId, ref: 'City' },
 		price: { type: Number, required: true },
 		date: { type: Date, required: true },
-		artist: {},
 		language: { type: String, required: true },
 		category: { type: Schema.Types.ObjectId, ref: 'Category' },
+		totalSeats: { type: Number, required: true },
+		seatsBooked: { type: Number, required: true, default: 0 },
 	},
 	{ timestamps: true }
 );
+
+export const Show = mongoose.model('Show', showSchema);
