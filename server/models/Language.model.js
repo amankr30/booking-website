3a0a377de13643b8d1149ad9bfd1643025d291bf
anasahmd <anasahmad0239@gmail.com>
2024-08-11 12:50:32 +0530

    Add langugae model

--- server/models/Language.model.js
+++ server/models/Language.model.js
@@ -2,7 +2,6 @@ import mongoose, { Schema } from 'mongoose';
 
 const languageSchema = new Schema({
 	name: { type: String, required: true, trim: true, unique: true },
-	code: { type: String, required: true },
 });
 
-export const Category = mongoose.model('Category', categorySchema);
+export const Language = mongoose.model('Language', languageSchema);
--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,14 +1,17 @@
 import mongoose, { Schema } from 'mongoose';
 
-const userSchema = new Schema({
-	fullName: {},
-	avatar: {},
-	password: {
-		type: String,
-		required: [true, 'Password is required'],
+const userSchema = new Schema(
+	{
+		fullName: {},
+		avatar: {},
+		password: {
+			type: String,
+			required: [true, 'Password is required'],
+		},
+		city: {},
+		role: {},
+		bookings: {},
+		shows: {},
 	},
-	city: {},
-	role: {},
-	bookings: {},
-	shows: {},
-});
+	{ timestamps: true }
+);
