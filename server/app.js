8f78da4e69c2720625606e57cf1f9646739b8885
anasahmd <anasahmad0239@gmail.com>
2024-08-14 14:39:37 +0530

    Add authentication using jwt

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { ApiError } from './utils/ApiError.js';
 import errorHandler from './utils/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -15,14 +16,17 @@ app.use(
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
+app.use(cookieParser());
 
 import cityRouter from './routes/city.route.js';
 import categoryRouter from './routes/category.route.js';
 import languageRouter from './routes/language.route.js';
+import userRouter from './routes/user.route.js';
 
 app.use('/api/v1/city', cityRouter);
 app.use('/api/v1/category', categoryRouter);
 app.use('/api/v1/language', languageRouter);
+app.use('/api/v1/user', userRouter);
 
 app.use((req, res, next) => {
 	next(new ApiError(404, 'Not Found'));
