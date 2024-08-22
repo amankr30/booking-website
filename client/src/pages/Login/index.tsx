72f84567e275f00834c9e4b1b55e35fffc4eb4d2
anasahmd <anasahmad0239@gmail.com>
2024-08-22 09:10:28 +0530

    Add redux and login

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -2,11 +2,51 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import Input from '../../components/form/Input';
 import Button from '../../components/form/Button';
+import { useState } from 'react';
+import { loggednInUserState, login } from '../../redux/authSlice';
+import { useAppDispatch } from '../../hooks/redux';
+
+interface LoginFormInputs {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm<LoginFormInputs>();
+  const [error, setError] = useState<unknown>('');
+  const dispatch = useAppDispatch();
+
+  const handleLogin = async (data: LoginFormInputs) => {
+    setError('');
+    const backendUri: string = import.meta.env.VITE_BACKEND_URI as string;
+    try {
+      const session = await fetch(`${backendUri}/api/v1/user/login`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+      if (!session.ok) {
+        const data = await session.json();
+        const message = data.message;
+        return setError(message);
+      }
+      const response = await session.json();
+
+      const userData: loggednInUserState = response.data.user;
+      if (userData) {
+        dispatch(login(userData));
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
   return (
     <div className="flex items-center justify-center w-full">
+      {`${error}`}
       <div
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
@@ -24,12 +64,12 @@ const Login = () => {
           </Link>
         </p>
 
-        <form className="mt-5">
+        <form className="mt-5" onSubmit={handleSubmit(handleLogin)}>
           <div className="space-y-5">
             <Input
               placeholder="Enter your email"
               type="email"
-              {...register('name', {
+              {...register('email', {
                 required: true,
                 validate: {
                   matchPatern: (value) =>
